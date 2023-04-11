6898d9eb62e4d63725a92a072c1364dda3ac5b7c
ChanHwan <song960705@naver.com>
2023-04-12 01:25:40 +0900

    feat: prefix 삽입 후 커서를 맨 뒤로 이동

--- content_scripts.js
+++ content_scripts.js
@@ -3,41 +3,44 @@ window.onload = () => {
     let chatframe = document.getElementById("chatframe");
     let el;
     let inputContainer;
+    let _window;
 
     //새 창에서 새 탭 열기 -> chatframe이 존재하지 않음
     //null값을 체크해 inputContainer를 document에서 찾을지, el에서 찾을지 지정
     if (chatframe == null) {
       inputContainer = document.querySelector("#input");
+      _window = window;
     } else {
       el = chatframe.contentWindow.document;
       inputContainer = el.querySelector("#input");
+      _window = chatframe.contentWindow;
     }
     let input = inputContainer.children[1];
 
     // focus 이벤트
     inputContainer.addEventListener("focusin", function (e) {
       e.preventDefault();
-      addPrefix(inputContainer, input);
+      addPrefix(inputContainer, input, _window);
     });
 
     // click 이벤트
     inputContainer.addEventListener("click", function (e) {
       e.preventDefault();
-      addPrefix(inputContainer, input);
+      addPrefix(inputContainer, input, _window);
     });
 
     // enter 이벤트
     inputContainer.addEventListener("keyup", function (e) {
       if (e.keyCode === 13 && inputContainer.hasAttribute("focused")) {
         e.preventDefault();
-        addPrefix(inputContainer, input);
+        addPrefix(inputContainer, input, _window);
       }
     });
   }, 2000);
 };
 
 // prefix 삽입 함수
-function addPrefix(inputContainer, input) {
+function addPrefix(inputContainer, input, _window) {
   chrome.storage.sync.get(["prefix"], function (result) {
     let prefix = result.prefix;
     let inputText = input.innerText;
@@ -49,5 +52,15 @@ function addPrefix(inputContainer, input) {
     inputContainer.setAttribute("has-text", true);
     input.removeAttribute("aria-invalid");
     input.innerText = `${prefix}`;
+
+    // 커서를 맨 뒤로 이동 -- start
+    const range = document.createRange();
+    range.setStart(input.childNodes[0], prefix.length);
+    range.setEnd(input.childNodes[0], prefix.length);
+
+    const selection = _window.document.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    // 커서를 맨 뒤로 이동 -- end
   });
 }
