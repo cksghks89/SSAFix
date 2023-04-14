9247f503b571714d9c5e0703ee3640b7d2d468ea
ChanHwan <song960705@naver.com>
2023-04-14 19:20:27 +0900

    fix: 오류발생 가능성이 있는 부분 예외처리

--- content_scripts.js
+++ content_scripts.js
@@ -1,76 +1,76 @@
 window.onload = () => {
-  let loadTimeCnt = 0;
-  let interval = setInterval(() => {
-    // 페이지 로딩 후 20초 경과시 interval 종료
-    if (loadTimeCnt++ >= 100) clearInterval(interval);
+  setInterval(() => {
+    try {
+      let chatframe = document.getElementById("chatframe");
+      let el;
+      let inputContainer;
+      let _window;
 
-    let chatframe = document.getElementById("chatframe");
-    let el;
-    let inputContainer;
-    let _window;
+      //새 창에서 새 탭 열기 -> chatframe이 존재하지 않음
+      //null값을 체크해 inputContainer를 document에서 찾을지, el에서 찾을지 지정
+      if (chatframe == null) {
+        inputContainer = document.querySelector("#input");
+        _window = window;
+      } else {
+        el = chatframe.contentWindow.document;
+        inputContainer = el.querySelector("#input");
+        _window = chatframe.contentWindow;
+      }
+      let input = inputContainer.children[1];
 
-    //새 창에서 새 탭 열기 -> chatframe이 존재하지 않음
-    //null값을 체크해 inputContainer를 document에서 찾을지, el에서 찾을지 지정
-    if (chatframe == null) {
-      inputContainer = document.querySelector("#input");
-      _window = window;
-    } else {
-      el = chatframe.contentWindow.document;
-      inputContainer = el.querySelector("#input");
-      _window = chatframe.contentWindow;
-    }
-    let input = inputContainer.children[1];
-
-    // focus 이벤트
-    inputContainer.addEventListener("focusin", function (e) {
-      e.preventDefault();
-      addPrefix(inputContainer, input, _window);
-    });
-
-    // click 이벤트
-    inputContainer.addEventListener("click", function (e) {
-      e.preventDefault();
-      addPrefix(inputContainer, input, _window);
-    });
-
-    // enter 이벤트
-    inputContainer.addEventListener("keyup", function (e) {
-      if (e.keyCode === 13 && inputContainer.hasAttribute("focused")) {
+      // focus 이벤트
+      inputContainer.addEventListener("focusin", function (e) {
         e.preventDefault();
         addPrefix(inputContainer, input, _window);
-      }
-    });
-  }, 200);
+      });
+
+      // click 이벤트
+      inputContainer.addEventListener("click", function (e) {
+        e.preventDefault();
+        addPrefix(inputContainer, input, _window);
+      });
+
+      // enter 이벤트
+      inputContainer.addEventListener("keyup", function (e) {
+        if (e.keyCode === 13 && inputContainer.hasAttribute("focused")) {
+          e.preventDefault();
+          addPrefix(inputContainer, input, _window);
+        }
+      });
+    } catch (err) {}
+  }, 500);
 };
 
 // prefix 삽입 함수
 function addPrefix(inputContainer, input, _window) {
-  // On Off 상태 획득
-  chrome.storage.local.get(["toggleState"], function (toggleResult) {
-    // unchecked or undefined 시 리턴
-    if (toggleResult.toggleState != true) return;
+  try {
+    // On Off 상태 획득
+    chrome.storage.local.get(["toggleState"], function (toggleResult) {
+      // unchecked or undefined 시 리턴
+      if (toggleResult.toggleState != true) return;
 
-    chrome.storage.local.get(["prefix"], function (result) {
-      let prefix = result.prefix;
-      let inputText = input.innerText;
+      chrome.storage.local.get(["prefix"], function (result) {
+        let prefix = result.prefix;
+        let inputText = input.innerText;
 
-      if (!prefix || inputText) {
-        return;
-      }
+        if (!prefix || inputText) {
+          return;
+        }
 
-      inputContainer.setAttribute("has-text", true);
-      input.removeAttribute("aria-invalid");
-      input.innerText = `${prefix}`;
+        inputContainer.setAttribute("has-text", true);
+        input.removeAttribute("aria-invalid");
+        input.innerText = `${prefix}`;
 
-      // 커서를 맨 뒤로 이동 -- start
-      const range = document.createRange();
-      range.setStart(input.childNodes[0], prefix.length);
-      range.setEnd(input.childNodes[0], prefix.length);
+        // 커서를 맨 뒤로 이동 -- start
+        const range = document.createRange();
+        range.setStart(input.childNodes[0], prefix.length);
+        range.setEnd(input.childNodes[0], prefix.length);
 
-      const selection = _window.document.getSelection();
-      selection.removeAllRanges();
-      selection.addRange(range);
-      // 커서를 맨 뒤로 이동 -- end
+        const selection = _window.document.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        // 커서를 맨 뒤로 이동 -- end
+      });
     });
-  });
+  } catch (err) {}
 }
--- money_generator_scripts.js
+++ money_generator_scripts.js
@@ -1,48 +1,50 @@
 window.onload = () => {
-  //패턴 " [한글_숫자한글_한글] "
-  const pattern = /\[[\uAC00-\uD7AF]+_+[\uAC00-\uD7AF\d]+[\uAC00-\uD7AF]+_[\uAC00-\uD7AF]+\]/;
-  //싸피 지역
-  const area = ["대전", "부울경", "서울", "구미", "광주"];
+  try {
+    //패턴 " [한글_숫자한글_한글] "
+    const pattern = /\[[\uAC00-\uD7AF]+_+[\uAC00-\uD7AF\d]+[\uAC00-\uD7AF]+_[\uAC00-\uD7AF]+\]/;
+    //싸피 지역
+    const area = ["대전", "부울경", "서울", "구미", "광주"];
 
-  chrome.storage.local.get(["prefix"], function (result) {
-    let prefix = result.prefix;
-    console.log(prefix);
+    chrome.storage.local.get(["prefix"], function (result) {
+      let prefix = result.prefix;
+      console.log(prefix);
 
-    if (!prefix) {
-      return;
-    }
-    //패턴이 [지역_05반_이름] 맞다면
-    if (pattern.test(prefix)) {
-      let spT = prefix.replace("[", "").replace("]", "").split("_");
-      let areaCheck = false;
-
-      //입력한 지역이 싸피 지역인지 체크
-      for (let i = 0; i < area.length; i++) {
-        if (area[i] == spT[0]) {
-          areaCheck = true;
-          break;
-        }
+      if (!prefix) {
+        return;
       }
+      //패턴이 [지역_05반_이름] 맞다면
+      if (pattern.test(prefix)) {
+        let spT = prefix.replace("[", "").replace("]", "").split("_");
+        let areaCheck = false;
 
-      //loactionInput의 default 값 공백
-      let locationInput = "";
-      let nameInput = document.querySelector("input[name='name']");
-      //만약 area 배열에 있는 값이 아니면 패스
-      if (areaCheck) locationInput = document.querySelector("input[name='location']");
-      let classNumInput = document.querySelector("input[name='classNum']");
+        //입력한 지역이 싸피 지역인지 체크
+        for (let i = 0; i < area.length; i++) {
+          if (area[i] == spT[0]) {
+            areaCheck = true;
+            break;
+          }
+        }
 
-      //input value 값 설정
-      nameInput.value = spT[2];
-      locationInput.value = spT[0];
-      classNumInput.value = parseInt(spT[1].replace(/[^0-9]/gi, ""), 10);
+        //loactionInput의 default 값 공백
+        let locationInput = "";
+        let nameInput = document.querySelector("input[name='name']");
+        //만약 area 배열에 있는 값이 아니면 패스
+        if (areaCheck) locationInput = document.querySelector("input[name='location']");
+        let classNumInput = document.querySelector("input[name='classNum']");
 
-      //이벤트 생성  { bubbles: true } 옵션을 추가하여, 이벤트가 상위 요소로 전파되도록 설정
-      let event = new Event("change", { bubbles: true });
+        //input value 값 설정
+        nameInput.value = spT[2];
+        locationInput.value = spT[0];
+        classNumInput.value = parseInt(spT[1].replace(/[^0-9]/gi, ""), 10);
 
-      //이벤트 강제 발생 (onChange)
-      nameInput.dispatchEvent(event);
-      locationInput.dispatchEvent(event);
-      classNumInput.dispatchEvent(event);
-    }
-  });
+        //이벤트 생성  { bubbles: true } 옵션을 추가하여, 이벤트가 상위 요소로 전파되도록 설정
+        let event = new Event("change", { bubbles: true });
+
+        //이벤트 강제 발생 (onChange)
+        nameInput.dispatchEvent(event);
+        locationInput.dispatchEvent(event);
+        classNumInput.dispatchEvent(event);
+      }
+    });
+  } catch (err) {}
 };
